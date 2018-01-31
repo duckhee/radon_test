492b6a23ca6e1d6e1862e83dbbc9280dca3ab0db
duckheewon <fain9301@yahoo.com>
2018-01-31 11:06:01 +0900

    testing http status connection teting

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,46 +1,57 @@
-
 var express = require('express');
 var router = express.Router();
 var radon_controller = require('../controller/data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-router.get('/insert', function(req, res, next){
-  var radon_sensor = req.query.sensor_name || req.param.sensor_name || req.params.sensor_name;
-  var radon_value = req.query.sensor_value || req.param.sensor_value || req.params.sensor_value;
-  var radon_info = {
-    name:radon_sesor,
-    value:radon_value
-  };
-  radon_controller.insert(radon_info, function(row, err){
-    if(row){
+router.get('/insertdata', function(req, res, next) {
+    var radon_sensor = req.query.sensor_name || req.param.sensor_name || req.params.sensor_name;
+    var radon_value = req.query.sensor_value || req.param.sensor_value || req.params.sensor_value;
+    var radon_info = {
+        name: radon_sesor,
+        value: radon_value
+    };
+    radon_controller.insert(radon_info, function(row, err) {
+        if (row) {
+            console.log('insert data : ', row);
+            res.redirect('/');
+        } else if (err) {
+            console.log('error : ', err);
+            res.status(500);
 
-    }else if(err){
+        } else {
+            console.log('null');
+            res.status(404);
 
-    }else{
-
-    }
-  });
+        }
+    });
 
 });
 
-router.get('/get', function(req, res, next){
-  var radon_sensor = req.query.sensor_name || req.params.sensor_name || req.param.sensor_name;
-  var radon_info = {
-    name : radon_sensor
-  };
-  radon_controller.findlist(radon_info, function(row, err){
-    if(row){
+router.get('/getdata', function(req, res, next) {
+    var radon_sensor = req.query.sensor_name || req.params.sensor_name || req.param.sensor_name;
+    var radon_info = {
+        name: radon_sensor
+    };
+    radon_controller.findlist(radon_info, function(row, err) {
+        if (row) {
+            console.log('find list : ', row);
+            res.json(row);
+        } else if (err) {
+            console.log('error : ', err);
+            res.json(err);
+        } else {
+            console.log('null');
+            res.json('null');
+        }
+    });
 
-    }else if(err){
+});
 
-    }else{
-
-    }
-  });
+router.get('get', function(req, res, next) {
 
 });
 
